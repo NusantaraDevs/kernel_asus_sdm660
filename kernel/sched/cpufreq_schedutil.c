90ec43df77b51244de082390bcc67544b653775d
Ahmad Thoriq Najahi <najahiii@outlook.co.id>
2019-11-08 03:59:15 +0700

    cpufreq: schedutil: Decrease up_rate_limit_us to 1000
    Signed-off-by: Ahmad Thoriq Najahi <najahiii@outlook.co.id>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -821,7 +821,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	}
 
         /* Hard-code some sane rate-limit values */
-        tunables->up_rate_limit_us = 5000;
+        tunables->up_rate_limit_us = 1000;
         tunables->down_rate_limit_us = 20000;
 
 	tunables->iowait_boost_enable = false;
