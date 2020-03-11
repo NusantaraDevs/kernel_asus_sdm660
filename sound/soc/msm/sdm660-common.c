d490f2e0644313ca68dacf746e2ea48946bb1c7d
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-03-11 21:20:20 +0530

    Input: misc: Import Semtech SX9310 SAR Driver
    * Based On 060 Kernel Source
    
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- sound/soc/msm/sdm660-common.c
+++ sound/soc/msm/sdm660-common.c
@@ -33,6 +33,10 @@
 #define DEFAULT_MCLK_RATE 9600000
 #define MSM_LL_QOS_VALUE 300 /* time in us to ensure LPM doesn't go in C3/C4 */
 
+#ifdef CONFIG_INPUT_SX9310
+extern void sar_switch(bool);
+#endif
+
 struct dev_config {
 	u32 sample_rate;
 	u32 bit_format;
@@ -2586,6 +2590,10 @@ int msm_mi2s_snd_startup(struct snd_pcm_substream *substream)
 
 		/* Huaqin add for config i2s tert dai for nxp pa by xudayi at 2018/03/03 start */
 		if (index == TERT_MI2S) {
+			#ifdef CONFIG_INPUT_SX9310
+			pr_debug("%s before open PA, close SAR!\n", __func__);
+			sar_switch(0);
+			#endif
 		    msm_cdc_pinctrl_select_active_state(pdata->tert_mi2s_gpio_p);
 			printk("daixianze %s tert_mi2s_gpio_p\n", __func__);
 		}
@@ -2637,6 +2645,10 @@ void msm_mi2s_snd_shutdown(struct snd_pcm_substream *substream)
 		{
 		    msm_cdc_pinctrl_select_sleep_state(pdata->tert_mi2s_gpio_p);
 			pr_err("daixianze %s tert_mi2s_gpio_p \n", __func__);
+			#ifdef CONFIG_INPUT_SX9310
+			pr_debug("%s after close PA, open SAR!\n", __func__);
+			sar_switch(1);
+			#endif
 		}
 		/* Huaqin add for config i2s tert dai for nxp pa by xudayi at 2018/03/03 end */
 
