unknownbaka/utopia_kernel_polaris

cpufreq: schedutil: Ignore CPU load older than WALT window size
The old code used to ignore CPU load older than 1 tick. Since the WALT
window size can be more than 1 tick (and is configurable), this can cause
an incorrect frequency drop in the middle of a WALT window. Use WALT window
size to ignore CPU load instead.

Change-Id: If42420f595ba9722866174c2cda8b6c2fdf34f92
Signed-off-by: Saravana Kannan <skannan@codeaurora.org>

kernel/sched/cpufreq_schedutil.c
@@ -253,7 +253,7 @@ static unsigned int sugov_next_freq_shared(struct sugov_cpu *sg_cpu,
 		 * idle now (and clear iowait_boost for it).
 		 */
 		delta_ns = last_freq_update_time - j_sg_cpu->last_update;
-		if (delta_ns > TICK_NSEC) {
+		if (delta_ns > sched_ravg_window) {
 			j_sg_cpu->iowait_boost = 0;
 			continue;
 		}

NusantaraDevs/kernel_asus_sdm660

Signed-off-by: celtare21 <celtare21@gmail.com>

kernel/sched/cpufreq_schedutil.c
@@ -360,7 +360,7 @@ static unsigned int sugov_next_freq_shared(struct sugov_cpu *sg_cpu, u64 time)
 		 * idle now (and clear iowait_boost for it).
 		 */
 		delta_ns = time - j_sg_cpu->last_update;
-		if (delta_ns > TICK_NSEC) {
+		if (delta_ns > walt_ravg_window) {
 			j_sg_cpu->iowait_boost = 0;
 			j_sg_cpu->iowait_boost_pending = false;
 			continue;