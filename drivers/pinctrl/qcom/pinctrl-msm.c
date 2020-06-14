cbf159b77c2eb39bd8474b412dc9443a4f6de592
itsmeley <itsmeley@nndrive.my.id>
2020-06-14 04:22:14 +0000

    Revert "BACKPORT: PM: wakeup_reason: add functions to query and clear wakeup reasons"
    This reverts commit 6cbcfacb6c65b090d0e18f644cbadf86649068b4.

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -768,7 +768,7 @@ static struct irq_chip msm_gpio_irq_chip = {
 	.irq_set_wake   = msm_gpio_irq_set_wake,
 };
 
-static bool msm_gpio_irq_handler(struct irq_desc *desc)
+static void msm_gpio_irq_handler(struct irq_desc *desc)
 {
 	struct gpio_chip *gc = irq_desc_get_handler_data(desc);
 	const struct msm_pingroup *g;
@@ -778,7 +778,6 @@ static bool msm_gpio_irq_handler(struct irq_desc *desc)
 	int handled = 0;
 	u32 val;
 	int i;
-	bool ret;
 
 	chained_irq_enter(chip, desc);
 
@@ -791,17 +790,16 @@ static bool msm_gpio_irq_handler(struct irq_desc *desc)
 		val = readl(pctrl->regs + g->intr_status_reg);
 		if (val & BIT(g->intr_status_bit)) {
 			irq_pin = irq_find_mapping(gc->irqdomain, i);
-			handled += generic_handle_irq(irq_pin);
+			generic_handle_irq(irq_pin);
+			handled++;
 		}
 	}
 
-	ret = (handled != 0);
 	/* No interrupts were flagged */
 	if (handled == 0)
-		ret = handle_bad_irq(desc);
+		handle_bad_irq(desc);
 
 	chained_irq_exit(chip, desc);
-	return ret;
 }
 
 /*
